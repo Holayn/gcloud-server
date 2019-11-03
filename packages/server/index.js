6969955d0823dded3c503ce5ddbb5044a4f75994
Holayn <kwwong1597@gmail.com>
2019-11-03 12:28:17 -0500

    server with inbox watch

---
+++ packages/server/index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const cors = require('cors');
+const winston = require('winston');
+const expressWinston = require('express-winston');
+const bodyParser = require('body-parser');
+const {google} = require('googleapis');
+
+const client = require('./google/auth');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console()
+  ],
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json()
+  ),
+  meta: true,
+  msg: "HTTP {{req.method}} {{req.url}}", 
+  expressFormat: false,
+  colorize: true,
+  ignoreRoute: function (req, res) { return false; }
+}));
+
+app.listen(process.env.PORT || 8080, () => {
+  console.info('Listening');
+  init();
+});
+
+app.get('/test', (req, res) => {
+  res.sendStatus(200);
+});
+
+// todo: need to call this every 7 days
+async function _watch(auth) {
+  const gmail = google.gmail({version: 'v1', auth});
+  await gmail.users.stop({
+    userId: 'me',
+  }).catch((err) => {
+    console.log(err);
+  });
+
+  const watchRes = await gmail.users.watch({
+    userId: 'me',
+    requestBody: {
+      topicName: 'projects/inbox-actions-1572042678872/topics/mailbox-changes',
+    },
+  }).catch((err) => {
+    console.log('The API returned an error: ' + err);
+  });
+
+  console.log(watchRes.data);
+}
+
+function init() {
+  watchGoogleInbox();
+}
+
+function watchGoogleInbox() {
+  client.executeAPI(_watch);
+  // call every day
+  setTimeout(() => {
+    watchGoogleInbox();
+  }, 86400000);
+}
